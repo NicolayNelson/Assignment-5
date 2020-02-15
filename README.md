ffdd25360a99c8102ff09ad45cb4371c05847704
NicolayNelson <59945806+NicolayNelson@users.noreply.github.com>
2020-02-14 18:00:23 -0800

    Finished question #3 and added a README file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Assignment-5
+Put arrays in functions to give a range a sum and to flip numbers.
--- script.js
+++ script.js
@@ -48,3 +48,22 @@ console.log(sum(range2(1, 10)));
 // reverseArrayInPlace(arrayValue);
 // console.log(arrayValue); // outputs [5, 4, 3, 2, 1]
 
+const reverseArray = function(array) {
+    let output = [];
+    for (let i = array.length - 1; i >= 0; i--) {
+      output.push(array[i]);
+    }
+    return output;
+  }
+  
+  const reverseArrayInPlace = function(array) {
+    for (let i = 0; i < Math.floor(array.length / 2); i++) {
+      let old = array[i];
+      array[i] = array[array.length - 1 - i];
+      array[array.length - 1 - i] = old;
+    }
+    return array;
+  }
+  
+  console.log(reverseArray(["A", "B", "C"]));
+  console.log(reverseArrayInPlace([1, 2, 3, 4, 5]));
